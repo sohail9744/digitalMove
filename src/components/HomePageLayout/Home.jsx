c5082fb61d9d9e28893e405d6bbc51852b549b84
Mohammad Sohail Khan <mohammadsohailkhan999@gmail.com>
2023-07-21 20:14:03 +0530

    added landing page with description and add button

--- src/components/HomePageLayout/Home.jsx
+++ src/components/HomePageLayout/Home.jsx
@@ -1,56 +1,75 @@
-import Footer from "../CommonLayout/Footer"
-import NavBar from "../CommonLayout/NavBar"
+import Footer from "../CommonLayout/Footer";
+import NavBar from "../CommonLayout/NavBar";
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
+import { Typewriter, Cursor } from "react-simple-typewriter";
+// import HomeBannerImage from "../../Images/HomeBannerImage.jpg";
+import buildingWebsite from "./Icons/buildingWebsite.svg";
+import OurServices from "./Services.jsx";
+
 export default function Home() {
+  const text = Typewriter({
+    words: [
+      "Ecommerce Website",
+      "General Business Website",
+      "Portfolio Website",
+      "Personal Blog Website",
+      "Non-Profit Org Website",
+    ],
+    loop: {},
+    // blinker: "_",
+    // delay: 1000,
+    // blinkerDelay: 1000,
+  });
   return (
     <>
       <NavBar />
+      {/* Main Contianer */}
       <main className="mt-20">
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check navbar fixed postion</p>
-        <p>Scroll to check Footer Sticky postion</p>
+        {/* Background image container */}
+        <div
+          className="banner-container bg-cover bg-center"
+          // style={{
+          //   backgroundImage: `url(${HomeBannerImage})`,
+          // }}
+        >
+          {/* store text and image  */}
+          <div className="flex flex-row justify-between p-20">
+            <div>
+              <h2 className="text-3xl font-popins font-extrabold">
+                {/* eslint-disable-next-line react/no-unescaped-entities */}
+                Let's Build
+                <span style={{ color: "green", marginLeft: "8px" }}>
+                  {text}
+                </span>
+                <span>
+                  <Cursor cursorColor="red" />
+                </span>
+              </h2>
+              <div className="w-4/5 my-12 ">
+                <p className="font-popins break-words font-normal">
+                  Welcome to
+                  <span className="ml-1 font-bold">DigitalM0ve.com</span>, where
+                  innovation meets excellence! We are your trusted partner in
+                  unleashing the full potential of technology for your business.
+                  From cutting-edge web development and captivating designs to
+                  seamless software solutions and digital marketing prowess, we
+                  craft tailored strategies to elevate your brands online
+                  presence. Join us on a journey of growth and success as we
+                  build a seamless bridge between your dreams and digital
+                  reality.
+                </p>
+              </div>
+              <Button variant="contained" endIcon={<SendIcon />}>
+                Get started
+              </Button>
+            </div>
+            <div style={{ width: "197%" }}>
+              <img src={buildingWebsite} alt="not found" />
+            </div>
+          </div>
+        </div>
+        <OurServices />
       </main>
       <Footer />
     </>
---
+++ src/components/HomePageLayout/Services.jsx
@@ -0,0 +1,10 @@
+const OurServices = () => {
+  return (
+    <>
+      <div className="bg-slate-500">
+        <p>Our services</p>
+      </div>
+    </>
+  );
+};
+export default OurServices
